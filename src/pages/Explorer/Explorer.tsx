c3c7b9b8c1c0771ae079aaf7d07eb3c2e62f1abe
Mateusz Sosnowski <mateusz.sosnowski@withintent.com>
2021-04-02 14:47:10 +0200

    Fix eslint and prettier error and warning highlights in IDE UI, change react import, add pages alias for imports

--- src/pages/Explorer/Explorer.tsx
+++ src/pages/Explorer/Explorer.tsx
@@ -1,8 +1,8 @@
-import React, { FC } from 'react';
+import * as React from 'react';
 
 import useStyles from './Explorer.styles';
 
-const Explorer: FC = () => {
+const Explorer: React.FC = () => {
   const classes = useStyles();
 
   return <div className={classes.container}>Explorer Page</div>;
