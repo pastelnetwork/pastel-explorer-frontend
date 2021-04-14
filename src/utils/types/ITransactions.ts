50b403fed826068b2aaf04e11107dd0156a1c651
Adrian Obetkał <adrian.obetkal@likims.com>
2021-04-14 08:25:55 +0200

    ITransaction interface adjustment

--- src/utils/types/ITransactions.ts
+++ src/utils/types/ITransactions.ts
@@ -1,9 +1,14 @@
+interface vType {
+  addresses: string;
+  amount: number;
+}
+
 export interface ITransaction {
   blockhash: string;
   blockindex: number;
   timestamp: number;
   total: number;
   txid: string;
-  vin: Array<{ addresses: string; amount: number }>;
-  vout: Array<{ addresses: string; amount: number }>;
+  vin: Array<vType>;
+  vout: Array<vType>;
 }
