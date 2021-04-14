0e8cb89faf2deb58d81fc96981da291ade63b19f
Adrian Obetkał <adrian.obetkal@likims.com>
2021-04-14 10:10:41 +0200

    network table added, connection interface added, connection url added

--- src/utils/constants/urls.ts
+++ src/utils/constants/urls.ts
@@ -2,5 +2,6 @@ export const BASE_URL = process.env.REACT_APP_EXPLORER_WEB_API_URL;
 export const SUMMARY_URL = '/ext/summary';
 export const SEARCH_URL = '/search';
 export const LAST_TRANSACTIONS_URL = '/ext/getlasttxs';
+export const CONNECTIONS_URL = '/ext/connections';
 
 export const TWITTER_URL = 'https://twitter.com/pastelnetwork';
---
+++ src/utils/types/IConnections.ts
@@ -0,0 +1,8 @@
+export interface IConnection {
+  address: string;
+  country: string;
+  createdAt: string;
+  protocol: string;
+  version: string;
+  _id: string;
+}
