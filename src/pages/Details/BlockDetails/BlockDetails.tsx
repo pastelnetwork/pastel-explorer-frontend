5d4c0cc5bd6b3882ad0af8be0e099d1c38e5866b
Adrian Obetkał <adrian.obetkal@likims.com>
2021-04-16 16:24:44 +0200

    api integration for transactions and blocks

--- src/pages/Details/BlockDetails/BlockDetails.tsx
+++ src/pages/Details/BlockDetails/BlockDetails.tsx
@@ -1,16 +1,17 @@
-import { useParams } from 'react-router-dom';
+import * as React from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 
 import { Grid } from '@material-ui/core';
 
 import Header from '@components/Header/Header';
-import Table from '@components/Table/Table';
+import Table, { RowsProps } from '@components/Table/Table';
 
-import {
-  blockHeaders,
-  transactionHeaders,
-  blockMockRows,
-  transactionsMockRows,
-} from './BlockDetails.helpers';
+import { useFetch } from '@utils/helpers/useFetch/useFetch';
+import * as URLS from '@utils/constants/urls';
+import { IBlock } from '@utils/types/IBlocks';
+import { formattedDate } from '@utils/helpers/date/date';
+
+import { blockHeaders } from './BlockDetails.helpers';
 
 interface ParamTypes {
   id: string;
@@ -19,23 +20,64 @@ interface ParamTypes {
 const BlockDetails = () => {
   const { id } = useParams<ParamTypes>();
 
-  return (
+  const [block, setBlock] = React.useState<IBlock | null>();
+  const [redirect, setRedirect] = React.useState(false);
+  const { fetchData } = useFetch<{ data: IBlock }>({
+    method: 'get',
+    url: `${URLS.BLOCK_URL}/${id}`,
+  });
+
+  React.useEffect(() => {
+    fetchData().then(response => {
+      if (!response || !response.data) {
+        setRedirect(true);
+      } else {
+        setBlock(response.data);
+      }
+    });
+  }, []);
+
+  const generateBlockTable = ({
+    height,
+    difficulty,
+    confirmations,
+    size,
+    nonce,
+    timestamp,
+  }: IBlock): RowsProps[] => {
+    return [
+      {
+        id: 1,
+        data: [
+          { id: 1, value: height },
+          { id: 2, value: difficulty },
+          { id: 3, value: confirmations },
+          { id: 4, value: size },
+          { id: 5, value: nonce },
+          { id: 6, value: formattedDate(timestamp) },
+        ],
+      },
+    ];
+  };
+
+  if (redirect) {
+    return <Redirect to="/404" />;
+  }
+
+  return block ? (
     <>
       <Header title="Block Details" />
       <Grid container direction="column">
         <Grid item>
-          <Table title={`PSL block: ${id}`} headers={blockHeaders} rows={blockMockRows} />
-        </Grid>
-        <Grid item>
           <Table
-            title="Latest Transactions"
-            headers={transactionHeaders}
-            rows={transactionsMockRows}
+            title={`PSL block: ${block.id}`}
+            headers={blockHeaders}
+            rows={generateBlockTable(block)}
           />
         </Grid>
       </Grid>
     </>
-  );
+  ) : null;
 };
 
 export default BlockDetails;
--- src/pages/Details/TransactionDetails/TransactionDetails.helpers.ts
+++ src/pages/Details/TransactionDetails/TransactionDetails.helpers.ts
@@ -15,34 +15,3 @@ export const recipientsHeaders: Array<HeaderType> = [
   { id: 1, header: 'Address' },
   { id: 2, header: 'Amount (PSL)' },
 ];
-
-export const transactionMockRows = [
-  {
-    id: 1,
-    data: [
-      { id: 1, value: '1' },
-      { id: 2, value: '000000014b1df3d3e2dbc70b80090279c9a63ad3728b2ae75578f6ac3d4dec24' },
-      { id: 3, value: '16th Apr 2021 07:53:28' },
-    ],
-  },
-];
-
-export const inputAddressMockRows = [
-  {
-    id: 1,
-    data: [
-      { id: 1, value: 'PtVRZ4aCKcPEchjsvr1vUo3tqRqDu58pZTr' },
-      { id: 2, value: '4875.00000000' },
-    ],
-  },
-];
-
-export const recipientsMockRows = [
-  {
-    id: 1,
-    data: [
-      { id: 1, value: 'Ptm6pogYcUQQ7qrDouQY5iHDpVytwh89xSX' },
-      { id: 2, value: '100.75054000' },
-    ],
-  },
-];
