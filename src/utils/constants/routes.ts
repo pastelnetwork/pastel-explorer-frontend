8f121f4b9a0cc10708e27d92935c24328069ab60
Adrian Obetkał <adrian.obetkal@likims.com>
2021-04-20 12:05:25 +0200

    transaction route reverted to tx

--- src/utils/constants/routes.ts
+++ src/utils/constants/routes.ts
@@ -6,6 +6,6 @@ export const INFO = '/info';
 export const BLOCKS = '/blocks';
 export const NOT_FOUND = '/not-found';
 
-export const TRANSACTION_DETAILS = '/transaction';
+export const TRANSACTION_DETAILS = '/tx';
 export const BLOCK_DETAILS = '/block';
 export const ADDRESS_DETAILS = '/address';
