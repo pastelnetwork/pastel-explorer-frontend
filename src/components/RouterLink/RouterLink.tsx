37f32776b539d547f857687529a1564756d9eee2
Mateusz Sosnowski <mateusz.sosnowski@withintent.com>
2021-04-21 10:57:34 +0200

    Optimize search component, delete any typings

--- src/components/RouterLink/RouterLink.tsx
+++ src/components/RouterLink/RouterLink.tsx
@@ -1,12 +1,18 @@
+import { CSSProperties } from '@material-ui/styles';
 import * as Styles from './RouterLink.styles';
 
 interface RouterLinkProps {
   route: string;
   value: string | number;
+  styles?: Partial<CSSProperties>;
 }
 
-const RouterLink: React.FC<RouterLinkProps> = ({ route, value }) => {
-  return <Styles.RouterLink to={`${route}`}>{value}</Styles.RouterLink>;
+const RouterLink: React.FC<RouterLinkProps> = ({ route, value, styles }) => {
+  return (
+    <Styles.RouterLink style={styles} to={`${route}`}>
+      {value}
+    </Styles.RouterLink>
+  );
 };
 
 export default RouterLink;
