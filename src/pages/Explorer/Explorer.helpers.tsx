e62576a198de8256ea3a7183c121f7fd88fbfe82
Adrian Obetkał <adrian.obetkal@likims.com>
2021-04-27 11:48:54 +0200

    lack of data case adjusted

--- src/pages/Explorer/Explorer.helpers.tsx
+++ src/pages/Explorer/Explorer.helpers.tsx
@@ -14,25 +14,27 @@ export const transformGeoLocationConnections = (
   locations: Array<INetworkPeers | INetworkMasternodes>,
   isMasternode: boolean,
 ): Array<MarkerProps> => {
-  const transformedLocations = locations.map(({ latitude, longitude, country, city, id, ip }) => {
-    const latLng = [latitude, longitude] as [number, number];
-    const defaultName = `${country} ${city !== 'N/A' ? `- ${city}` : ''}`;
-    const fill = isMasternode ? masternode : peer;
-    const data = {
-      type: isMasternode ? NODE_NAMES.supernode : NODE_NAMES.peer,
-      country,
-      city,
-      id,
-      ip,
-    };
+  const transformedLocations = locations.map(
+    ({ latitude = 0, longitude = 0, country = '', city = '', id = '', ip = '' }) => {
+      const latLng = [latitude, longitude] as [number, number];
+      const defaultName = `${country} ${city !== 'N/A' ? `- ${city}` : ''}`;
+      const fill = isMasternode ? masternode : peer;
+      const data = {
+        type: isMasternode ? NODE_NAMES.supernode : NODE_NAMES.peer,
+        country,
+        city,
+        id,
+        ip,
+      };
 
-    return {
-      latLng,
-      name: defaultName,
-      style: { fill, stroke: fill },
-      data: [data],
-    };
-  });
+      return {
+        latLng,
+        name: defaultName,
+        style: { fill, stroke: fill },
+        data: [data],
+      };
+    },
+  );
 
   return transformedLocations;
 };
@@ -40,8 +42,8 @@ export const transformGeoLocationConnections = (
 export const groupGeoLocationConnections = (locations: Array<MarkerProps>): Array<MarkerProps> => {
   const groupedLocations = locations.reduce((acc: Array<Array<MarkerProps>>, value) => {
     const sameLocationIndex = acc.findIndex(element => {
-      const hasSameLat = element[0].latLng[0] === value.latLng[0];
-      const hasSameLng = element[0].latLng[1] === value.latLng[1];
+      const hasSameLat = element[0]?.latLng[0] === value?.latLng[0];
+      const hasSameLng = element[0]?.latLng[1] === value?.latLng[1];
 
       return hasSameLat && hasSameLng;
     });
