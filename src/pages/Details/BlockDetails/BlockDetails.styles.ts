25e76c0cb7ef7eec3bdc95ac7bdde0dce5ef2b34
Adrian Obetkał <adrian.obetkal@likims.com>
2021-05-04 12:56:57 +0200

    block details header navigation added

---
+++ src/pages/Details/BlockDetails/BlockDetails.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { Typography as MuiTypography } from '@material-ui/core';
+
+export const Typography = styled(MuiTypography)`
+  flex-grow: 1;
+  margin: 0 5px;
+`;
