74a43100db4d81a0860b24428a30b72c150c3c06
Adrian Obetkał <adrian.obetkal@likims.com>
2021-05-07 14:28:13 +0200

    visual blocks added into blocks page above table

---
+++ src/pages/Blocks/BlockVisualization/BlockVisualization.styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+import { lighten } from '@material-ui/core';
+
+import themeVariant from '@theme/variants';
+
+export const BlockContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  color: ${themeVariant.custom.white};
+  text-align: center;
+  width: 125px;
+  height: 125px;
+  cursor: pointer;
+  background: repeating-linear-gradient(
+    ${themeVariant.palette.primary.main},
+    ${themeVariant.palette.primary.main} 0.018575%,
+    ${themeVariant.palette.secondary.main} 0.018575%,
+    ${themeVariant.palette.primary.main} 100%
+  );
+  transition: transform 0.2s ease-in;
+
+  &::before {
+    content: '';
+    width: 20px;
+    height: 125px;
+    position: absolute;
+    top: -12px;
+    left: -20px;
+    background-color: ${themeVariant.sidebar.background};
+    transform: skewY(50deg);
+    transform-origin: top;
+  }
+
+  &::after {
+    content: '';
+    width: 125px;
+    height: 24px;
+    position: absolute;
+    top: -24px;
+    left: -20px;
+    background-color: ${lighten(themeVariant.sidebar.background, 0.2)};
+    transform: skew(40deg);
+    transform-origin: top;
+  }
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+`;
--- src/pages/Blocks/Blocks.helpers.tsx
+++ src/pages/Blocks/Blocks.helpers.tsx
@@ -1,19 +1,51 @@
 import RouterLink from '@components/RouterLink/RouterLink';
 
 import * as ROUTES from '@utils/constants/routes';
-import { formattedDate } from '@utils/helpers/date/date';
+import { formattedDate, getCurrentUnixTimestamp } from '@utils/helpers/date/date';
 import { IBlock } from '@utils/types/IBlocks';
 
 import { BLOCK_ID_KEY, TRANSACTIONS_QTY_KEY, TIMESTAMP_BLOCKS_KEY } from './Blocks.columns';
+
+const BLOCK_ELEMENTS_COUNT = 8;
 
 export const DATA_FETCH_LIMIT = 20;
 export const DATA_OFFSET = 0;
 export const DATA_DEFAULT_SORT = 'DESC';
 
-export const transformBlocksData = (transactions: Array<IBlock>) =>
-  transactions.map(({ id, timestamp, transactionCount }) => ({
-    id,
-    [BLOCK_ID_KEY]: <RouterLink route={`${ROUTES.BLOCK_DETAILS}/${id}`} value={id} />,
-    [TRANSACTIONS_QTY_KEY]: transactionCount,
-    [TIMESTAMP_BLOCKS_KEY]: formattedDate(timestamp, { dayName: false }),
-  }));
+export const transformTableData = (transactions: Array<IBlock>) => {
+  return transactions.map(({ id, timestamp, transactionCount, height, size }) => {
+    const minutesAgo = Math.floor((getCurrentUnixTimestamp - timestamp) / 60);
+    return {
+      id,
+      [BLOCK_ID_KEY]: <RouterLink route={`${ROUTES.BLOCK_DETAILS}/${id}`} value={id} />,
+      [TRANSACTIONS_QTY_KEY]: transactionCount,
+      [TIMESTAMP_BLOCKS_KEY]: formattedDate(timestamp, { dayName: false }),
+      height,
+      size: `${(size / 1024).toFixed(2)} MB`,
+      minutesAgo: `${minutesAgo} minute${minutesAgo === 1 ? '' : 's'} ago`,
+    };
+  });
+};
+
+export interface TransformBlocksData {
+  id: string;
+  transactionCount: string;
+  height: number;
+  size: string;
+  minutesAgo: string;
+}
+
+export const transformBlocksData = (transactions: Array<IBlock>): Array<TransformBlocksData> => {
+  return transactions
+    .slice(0, BLOCK_ELEMENTS_COUNT)
+    .map(({ id, timestamp, transactionCount, height, size }) => {
+      const minutesAgo = Math.floor((getCurrentUnixTimestamp - timestamp) / 60);
+      return {
+        id,
+        transactionCount: `${transactionCount} transaction${transactionCount === 1 ? '' : 's'}`,
+        height,
+        size: `${(size / 1024).toFixed(2)} MB`,
+        minutesAgo: `${minutesAgo} minute${minutesAgo === 1 ? '' : 's'} ago`,
+      };
+    });
+};
