d396481be9297090147d680891ec06e19a989143
Igor <igorgark1583@gmail.com>
2021-06-08 02:27:29 -0500

    Fix transaction ui issues.

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -72,11 +72,43 @@ const Summary: React.FC = () => {
                     difference > 0 ? themeVariant.custom.green.dark : themeVariant.custom.red.dark
                   }`}
                 >
-                  <span>
-                    {`${difference > 0 ? '+' : ''}`}
-                    {difference}%
-                  </span>
                   Since yesterday
+                  <br />
+                  <span style={{ fontWeight: 'normal', marginTop: 8 }}>
+                    {`${difference > 0 ? '+' : ''}`}
+                    {difference}%&nbsp;
+                    {difference > 0 ? (
+                      <svg
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M4.23381 2.93331L1.08356 6.08356L4.37114e-07 5L5 4.37114e-07L10 5L8.91644 6.08356L5.76619 2.93331L5.76619 10L4.23381 10L4.23381 2.93331Z"
+                          fill="#00D097"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        width="10"
+                        height="10"
+                        viewBox="0 0 10 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M5.76619 7.06669L8.91644 3.91644L10 5L5 10L-2.18557e-07 5L1.08356 3.91644L4.23381 7.06669L4.23381 -2.52048e-07L5.76619 -1.85066e-07L5.76619 7.06669Z"
+                          fill="#FF754C"
+                        />
+                      </svg>
+                    )}
+                  </span>
                 </Styles.Percentage>
               )}
             </Styles.CardContent>
