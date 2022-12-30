4bde619a7afb2a6736a1908f6026a4655f2cc341
Tuan Nguyen <titan.ngvtuan@gmail.com>
2022-12-30 10:39:22 +0700

    added live update data for the Hashrate, Volume of transactions, and Incoming transactions on the Current Statistics page

--- src/pages/Statistics/NetworkStatistics/NetworkStatistics.tsx
+++ src/pages/Statistics/NetworkStatistics/NetworkStatistics.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, useContext, useEffect } from 'react';
 import { Skeleton } from '@material-ui/lab';
 
 import { TMiningInfo, TLineChartData } from '@utils/types/IStatistics';
@@ -8,24 +8,68 @@ import {
   generatePeriodToDropdownOptions,
 } from '@utils/helpers/statisticsLib';
 import * as URLS from '@utils/constants/urls';
-import { useDeferredData } from '@utils/helpers/useFetch/useFetch';
+import { useFetch, useDeferredData } from '@utils/helpers/useFetch/useFetch';
 import { periods } from '@utils/constants/statistics';
 import { LineChart } from '@components/Summary/LineChart';
 import { Dropdown } from '@components/Dropdown/Dropdown';
+import { BlockUnconfirmed } from '@utils/types/ITransactions';
+import { IBlock } from '@utils/types/IBlocks';
+import { SocketContext } from '@context/socket';
 
 import * as SummaryStyles from '@components/Summary/Summary.styles';
 import * as Styles from '@pages/CascadeAndSenseStatistics/CascadeAndSenseStatistics.styles';
 import * as StatisticsStyles from '../Statistics.styles';
+import {
+  transformBlocksData,
+  ITransformBlocksData,
+} from '../BlockStatistics/BlockStatistics.helpers';
+
+const BLOCK_ELEMENTS_COUNT = 8;
 
 const NetworkStatistics: React.FC = () => {
+  const socket = useContext(SocketContext);
   const [period, setPeriod] = useState(periods[2][0]);
+  const [blockElements, setBlockElements] = useState<Array<ITransformBlocksData>>([]);
+
+  const fetchUnconfirmedTxs = useFetch<{ data: BlockUnconfirmed[] }>({
+    method: 'get',
+    url: URLS.GET_UNCONFIRMED_TRANSACTIONS,
+  });
+
+  const fetchBlocksData = useFetch<{ data: Array<IBlock>; timestamp: number }>({
+    method: 'get',
+    url: URLS.BLOCK_URL,
+  });
+
+  const handleBlocksData = () => {
+    Promise.all([
+      fetchBlocksData.fetchData({ params: { limit: BLOCK_ELEMENTS_COUNT } }),
+      fetchUnconfirmedTxs.fetchData(),
+    ]).then(([blocksData]) => {
+      if (blocksData) {
+        setBlockElements(transformBlocksData(blocksData.data, blocksData.timestamp));
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleBlocksData();
+
+    socket.on('getUpdateBlock', () => {
+      handleBlocksData();
+    });
+
+    return () => {
+      socket.off('getUpdateBlock');
+    };
+  }, []);
 
   const { isLoading, data: chartData } = useDeferredData<{ data: TMiningInfo[] }, TLineChartData>(
     { method: 'get', url: `${URLS.GET_STATISTICS_HASHRATE}?period=${period}` },
     ({ data }) => transformHashrateInfo(data.sort((a, b) => a.timestamp - b.timestamp)),
     undefined,
     undefined,
-    [period],
+    [period, blockElements],
   );
 
   const handleDropdownChange = (
