8cf69aee877d1b52d5267585cc2f302ec3b123d3
Tuan Nguyen <titan.ngvtuan@gmail.com>
2023-01-11 15:22:58 +0700

    Add SWR library for Blocks

---
+++ src/hooks/useBlocks.ts
@@ -0,0 +1,23 @@
+import useSWRInfinite from 'swr/infinite';
+
+import { axiosGet } from '@utils/helpers/useFetch/useFetch';
+import * as URLS from '@utils/constants/urls';
+import { SortDirectionsType } from '@components/InfinityTable/InfinityTable';
+
+export default function useBlocks(
+  offset: number,
+  limit: number,
+  sortBy: string,
+  sortDirection: SortDirectionsType,
+  period: string,
+) {
+  const { data, isLoading } = useSWRInfinite(
+    () =>
+      `${URLS.BLOCK_URL}?offset=${offset}&limit=${limit}&sortBy=${sortBy}&sortDirection=${sortDirection}&period=${period}`,
+    axiosGet,
+  );
+  return {
+    swrData: data ? data[0] : null,
+    isLoading,
+  };
+}
